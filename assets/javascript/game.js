335e559c08523f25b7ec8d3107317a37ae5cb1b4
Gabe Quinn <gabe.quinn@icloud.com>
2019-09-30 10:03:14 -0400

    updated js have working game with values resetting on new game V3.0

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,14 +3,14 @@ $(document).ready(function () {
     var score = 0;
     var wins = 0;
     var losses = 0;
-    var rubyNum = Math.floor(Math.random() * 11 + 1);
-    var diamondNum = Math.floor(Math.random() * 11 + 1);
-    var emeraldNum = Math.floor(Math.random() * 11 + 1);
-    var topazNum = Math.floor(Math.random() * 11 + 1);
+    var rubyNum = Math.floor(Math.random() * 11) + 1;
+    var diamondNum = Math.floor(Math.random() * 11) + 1;
+    var emeraldNum = Math.floor(Math.random() * 11) + 1;
+    var topazNum = Math.floor(Math.random() * 11) + 1;
     $("#goal").text("Target: " + goalNum);
-
-
     $("#score").text("Score: " + score);
+    $("#wins").text("Wins: " + wins);
+    $("#losses").text("Losses: " + losses);
 
     score = parseInt(score);
     rubyNum = parseInt(rubyNum);
@@ -19,83 +19,103 @@ $(document).ready(function () {
     emeraldNum = parseInt(emeraldNum);
     // restart game function
     function restartGame() {
-        $("#goal").empty();
-        $("#score").empty();
         score = 0;
-        var goalNum = Math.floor(Math.random() * 101 + 19);
-        var rubyNum = Math.floor(Math.random() * 11 + 1);
-        var diamondNum = Math.floor(Math.random() * 11 + 1);
-        var emeraldNum = Math.floor(Math.random() * 11 + 1);
-        var topazNum = Math.floor(Math.random() * 11 + 1);
+        $("#score").text("Score: " + score);
+        goalNum = Math.floor(Math.random() * 101) + 19;
         $("#goal").text("Target: " + goalNum);
+        rubyNum = Math.floor(Math.random() * 11) + 1;
+        diamondNum = Math.floor(Math.random() * 11) + 1;
+        emeraldNum = Math.floor(Math.random() * 11) + 1;
+        topazNum = Math.floor(Math.random() * 11) + 1;
+        
+        score = parseInt(score);
+        rubyNum = parseInt(rubyNum);
+        diamondNum = parseInt(diamondNum);
+        topazNum = parseInt(topazNum);
+        emeraldNum = parseInt(emeraldNum);
+
+
         console.log("Ruby: " + rubyNum);
         console.log("Diamond: " + diamondNum);
         console.log("Topaz: " + topazNum);
         console.log("Emerald: " + emeraldNum);
+        console.log("Target: " + goalNum);
 
     };
 
-    // check to see if players total score has matched the beginning number if so game over add to wins
     function won() {
         wins++;
         $("#wins").text("Wins: " + wins);
+        $("#goal").empty();
+        $("#score").empty();
         restartGame();
     };
 
     function lost() {
         losses++;
         $("#losses").text("Losses: " + losses);
+        $("#goal").empty();
+        $("#score").empty();
         restartGame();
     };
 
     //click on ruby when clicked reveal random score that gets added to players total score 
     $("#red").click(function () {
         score = score + rubyNum;
-        $("#score").text(score);
+        $("#score").text("Score: " + score);
         if (score === goalNum) {
             won();
+            // restartGame();
         }
-        else if (score > goalNum) {
+        if (score > goalNum) {
             lost();
+            // restartGame();
         }
     });
 
     //click on diamond when clicked reveal random score that gets added to players total score 
     $("#blue").click(function () {
         score = score + diamondNum;
-        $("#score").text(score);
+        $("#score").text("Score: " + score);
         if (score === goalNum) {
             won();
+            // restartGame();
         }
-        else if (score > goalNum) {
+        if (score > goalNum) {
             lost();
+            // restartGame();
         }
     });
     //click on topaz when clicked reveal random score that gets added to players total score 
     $("#yellow").click(function () {
         score = score + topazNum;
-        $("#score").text(score);
+        $("#score").text("Score: " + score);
         if (score === goalNum) {
             won();
+            // restartGame();
         }
-        else if (score > goalNum) {
+        if (score > goalNum) {
             lost();
+            // restartGame();
         }
     });
     //click on emerald when clicked reveal random score that gets added to players total score 
 
     $("#green").click(function () {
         score = score + emeraldNum;
-        $("#score").text(score);
+        $("#score").text("Score: " + score);
         if (score === goalNum) {
             won();
+            // restartGame();
         }
-        else if (score > goalNum) {
+        if (score > goalNum) {
             lost();
+            // restartGame();
         }
     });
     console.log("Ruby: " + rubyNum);
     console.log("Diamond: " + diamondNum);
     console.log("Topaz: " + topazNum);
     console.log("Emerald: " + emeraldNum);
+    console.log("Target: " + goalNum);
 });
